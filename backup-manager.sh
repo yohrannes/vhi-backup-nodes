81ac4836029fb14e3949ee3db656bce84fd66823
yohrannes <51239326+yohrannes@users.noreply.github.com>
2023-10-28 14:13:59 -0300

    Update script

--- backup-manager.sh
+++ backup-manager.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/yohrannes/vhi-backup-nodes/main/backup-manager.sh
+# How do use
+# bash $PWD/backup-manager.sh CTID BACKUPID
 checkbackups() {
-    ctid=$1;
-    restore=$2;
-    uuid=$( (prlctl list -a || vzlist -a) | grep "$ctid" | tail -n 1 | grep -o '{[^}]*}' );
+    ctid=$1; restore=$2; uuid=$( (prlctl list -a || vzlist -a) | grep "$ctid" | tail -n 1 | grep -o '{[^}]*}' );
     if [[ "$uuid" == "" ]]; then
         echo -e '\e[91mSnapshot não encontrado\e[0m'
         sleep 4
         exit 0
     fi
-    backups=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $2}'));
-    datas=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $4}'));
-    horas=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $5}'));
+    backups=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $2}')); datas=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $4}')); horas=($(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $5}'));
     for ((i=0; i<=6; i++)); do
         if [[ ! "${datas[$i]}" == */* ]]; then
             datas[i]=""; horas[i]="";
         fi
     done;
     for ((i=0;i<=6;i++)); do
-        data_brasil[i]=$(date -d "${datas[$i]}" +"%d/%m/%Y");
-        hora_ajustada[i]=$(date -d "${horas[$i]} 3 hours ago" +"%H:%M:%S");
+        data_brasil[i]=$(date -d "${datas[$i]}" +"%d/%m/%Y"); hora_ajustada[i]=$(date -d "${horas[$i]} 3 hours ago" +"%H:%M:%S");
         if [[ "${horas[$i]}" < "18:00:00" ]]; then
             data_brasil[i]=$(date -d "${datas[$i]} 1 day ago" +"%d/%m/%Y");
         fi
@@ -33,11 +31,7 @@ checkbackups() {
     fi
 }
 restorenode () {
-    echo; echo -e 'SELECIONADA A OPÇÃO \e[32m'"$restore" - "${datas[$restore]}" "${horas[$restore]}"'\e[0m CONFIRMA? (Caso caso não aperte CTRL+C dentro de 15 segundos.)';sleep 15;  
-    echo; echo -e 'Restaurando para \e[91m'"${backups[$restore]}" "${datas[$restore]}" "${horas[$restore]}"'\e[0m';
-    echo "Restaurando para ${backups[$restore]} ${datas[$restore]}";
-    echo; vzctl stop "$ctid"; prlctl restore "$uuid" -t "${backups[$restore]}";
-    vzctl start "$ctid"; history -c;
+    echo; echo -e 'SELECIONADA A OPÇÃO \e[32m'"$restore" - "${datas[$restore]}" "${horas[$restore]}"'\e[0m CONFIRMA? (Caso caso não aperte CTRL+C dentro de 15 segundos.)';sleep 15; echo; echo -e 'Restaurando para \e[91m'"${backups[$restore]}" "${datas[$restore]}" "${horas[$restore]}"'\e[0m'; echo "Restaurando para ${backups[$restore]} ${datas[$restore]}"; echo; vzctl stop "$ctid"; prlctl restore "$uuid" -t "${backups[$restore]}"; vzctl start "$ctid"; history -c;
 }
 if [[ -n "$1" || -n "$2" ]]; then
     checkbackups "$1" "$2"

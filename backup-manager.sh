41f4ccaff1f6845e01d3083d8a7eb349302779da
yohrannes <51239326+yohrannes@users.noreply.github.com>
2023-11-02 08:45:52 -0300

    Change script to english

--- backup-manager.sh
+++ backup-manager.sh
@@ -4,40 +4,37 @@ checkbackups(){
     
     IFS=" " read -r -a uuid <<< "$( (prlctl list -a || vzlist -a) | grep "$ctid" | tail -n 1 | grep -o '{[^}]*}')"
 
-    if [[ "$uuid" == "" ]]; then echo -e '\e[91mSnapshot não encontrado\e[0m'; sleep 4; exit 0; fi;
+    if [[ "$uuid" == "" ]]; then echo -e '\e[91m\nSnapshot not found\n\e[0m'; sleep 4; exit 0; fi;
     
     IFS=" " read -r -a backups <<< "$(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $2}')";
     IFS=" " read -r -a datas <<< "$(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $4}')";
     IFS=" " read -r -a horas <<< "$(prlctl backup-list "$uuid" 2>&1 | grep -v 'Warning:' | grep -v Backup | tail -n 7 | awk '{print $5}')";
 
-    echo -e '\nBackups disponiveis para CTID: \e[32m'"$ctid"'\e[0m UUID: \e[32m'"$uuid"'\e[0m\n';
+    echo -e '\nDisponible backups for CTID: \e[32m'"$ctid"'\e[0m UUID: \e[32m'"$uuid"'\e[0m\n';
 
-    for ((i=0; i<=6; i++)); do if [[ ! "${datas[$i]}" == */* ]]; then datas[i]=""; horas[i]=""; fi; done;
-    for ((i=0;i<=6;i++)); do
+    for ((i=0; i<=6; i++)); do
+        if [[ ! "${datas[$i]}" == */* ]]; then
+            datas[i]=""; horas[i]="";
+        fi;
         if [[ "${datas[$i]}" != "" ]]; then
-            data_brasil[i]=$(date -d "${datas[$i]}" +"%d/%m/%Y");
-            hora_ajustada[i]=$(date -d "${horas[$i]} 3 hours ago" +"%H:%M:%S");
-            if [[ "${horas[$i]}" < "18:00:00" ]]; then
-                data_brasil[i]=$(date -d "${datas[$i]} 1 day ago" +"%d/%m/%Y");
-            fi;
-        echo -e '\e[32m'$i - "${data_brasil[$i]}" "${hora_ajustada[$i]}"'\e[0m';
+            echo -e '\e[32m'$i - "${datas[$i]}" "${horas[$i]}"'\e[0m';
         fi;
     done;
     echo;
     if [[ "$2" == "" ]]; then exit 0; else restorenode "$2";fi
-    
+
 };
 
 restorenode(){ 
 
-    echo -e '\nSELECIONADA A OPÇÃO \e[32m'"$restore" - "${data_brasil[$restore]}" "${hora_ajustada[$restore]}"'\e[0m CONFIRMA? (Caso caso não aperte CTRL+C dentro de 15 segundos.)\n';sleep 15;
+    echo -e '\nSELECTED THE OPTION \e[32m'"$restore" - "${datas[$restore]}" "${horas[$restore]}"'\e[0m CONFIRM? (If not tap CTRL+C in 15 seconds.)\n';sleep 15;
 
-    if [[ "${data_brasil[$restore]}" == "" ]]; then
-        echo -e '\e[91mSnapshot selecionado não encontrado\n\e[0m'
+    if [[ "${datas[$restore]}" == "" ]]; then
+        echo -e '\e[91mSelected snapshot not found\n\e[0m'
     else
-        echo -e '\nRestaurando para \e[91m'"${backups[$restore]}" "${data_brasil[$restore]}" "${hora_ajustada[$restore]}"'\e[0m\n';
+        echo -e '\nRestoring from \e[91m'"${backups[$restore]}" "${datas[$restore]}" "${horas[$restore]}"'\e[0m\n';
         vzctl stop "$ctid"; prlctl restore "$uuid" -t "${backups[$restore]}";
-        vzctl start "$ctid"; echo -e '\n\e[91mRestauração finalizada\e[0m \n\n Escrito por Yohrannes Santos Bigoli.\n'; history -c;
+        vzctl start "$ctid"; echo -e '\n\e[91mRestore finish\e[0m \n\n Writen by Yohrannes Santos Bigoli.\n'; history -c;
     fi;
 };
 
